f52c2bf4d86ebd642b2f364de414bfc228cddd02
finlaymcdonald <joseph.mcdonald@hammerheadvr.com>
2016-01-11 14:02:21 +0000

    added project

---
+++ GazeTracking/Assets/LookAtController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRStandardAssets.Utils;
+
+public class LookAtController : MonoBehaviour {
+
+	public VRInteractiveItem interactive;
+	public int rotationRate;
+
+	
+	void Update(){
+		if(interactive.IsOver){
+			Debug.Log ("isOver");
+			this.gameObject.transform.Rotate (Vector3.up * Time.deltaTime * rotationRate);
+		}
+	}
+}
